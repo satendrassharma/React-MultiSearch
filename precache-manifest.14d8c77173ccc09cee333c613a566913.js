2a5b2620b9960ab2f216371b63c4de779f5487e5
Satendra sharma <satendrasharma6991@gmail.com>
2019-10-22 10:59:27 +0530

    Updates

---
+++ precache-manifest.14d8c77173ccc09cee333c613a566913.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbc3d85fdcfb42a8b5de86464152d6a",
+    "url": "/React-MultiSearch/index.html"
+  },
+  {
+    "revision": "974eb5fd4d6fed7bd477",
+    "url": "/React-MultiSearch/static/css/main.225617d6.chunk.css"
+  },
+  {
+    "revision": "1855bd3eecbc8e18637b",
+    "url": "/React-MultiSearch/static/js/2.b5586dd2.chunk.js"
+  },
+  {
+    "revision": "974eb5fd4d6fed7bd477",
+    "url": "/React-MultiSearch/static/js/main.700f4422.chunk.js"
+  },
+  {
+    "revision": "50d5cb44bf26951d4450",
+    "url": "/React-MultiSearch/static/js/runtime-main.35665785.js"
+  }
+]);
